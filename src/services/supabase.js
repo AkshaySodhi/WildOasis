051a03ef0eb19bb37407200f6505f4a91484ddb9
AkshaySodhi <95957791+AkshaySodhi@users.noreply.github.com>
2024-08-31 22:22:09 +0530

    scroll hide

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+export const supabaseUrl = 'https://doxutjfhkkpejugusuop.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRveHV0amZoa2twZWp1Z3VzdW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwMDA5MjEsImV4cCI6MjA0MDU3NjkyMX0.qy0f5LW9uRZHphQAvDuXKyUKEss7CoZ5ECXPPnkuGos";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
